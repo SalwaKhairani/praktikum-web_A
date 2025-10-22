ad8f55cdeb3284b38765fee598696048714447d1
SalwaKhairani <salwakhairani745@gmail.com>
2025-10-22 16:10:06 +0800

    Delete 02_assignment directory

--- 02_assignment/soal1.php
+++
@@ -1,7 +0,0 @@
-<?php
-$saldoAwal = 1000000;
-$bunga = 0.0025;
-$bulan = 11;
-$saldoAkhir = $saldoAwal * pow((1 + $bunga), $bulan);
-echo "Saldo akhir setelah ".$bulan." bulan adalah : Rp. ".round($saldoAkhir).";";
-?>
